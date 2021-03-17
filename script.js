5f8a5f7260fd0913e18aa56a9686fd3615d6c009
vivimortensen <vivi4531@stud.kea.dk>
2021-03-17 11:58:46 +0100

    1703 basic html for configurator + fetch svg data

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict"; 
+
+//Model of all features 
+const features = {
+    stickerDramaqueen: false, 
+    stickerHeart: false, 
+    stickerRainbow: false 
+}; 
+
+//Global variable
+let elementToPaint; 
+
+window.addEventListener("DOMContentLoaded", init); 
+
+async function init(){
+    console.log("It works 👍🏻"); 
+
+    //Fetch svg 
+    let response = await fetch ("iphone_basic.svg"); 
+    let mySvgData = await response.text(); 
+    document.querySelector("#iphone-basic").innerHTML = mySvgData; 
+}
